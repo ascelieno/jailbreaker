5d6a548d0045cc0ef14cbbdf6fe22d594d620f21
Jonas Oberg <josobg01@b-an01.hpc2n.umu.se>
2023-10-09 20:39:50 +0200

    Update all files in the directory

--- load.sh
+++ load.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # Clear the environment from any previously loaded modules
-module purge > /dev/null
+module purge > /dev/null 2>&1
 
 # Load necessary modules
 module load GCCcore/11.3.0 Python/3.10.4 CUDA/11.6.0
 
-
 # Activate the Python virtual environment
 source /proj/nobackup/hpc2n2023-115/env/bin/activate
 
-echo "Instantiating llama"
+echo "Loading llama..."
 
 # Run the torch command with srun
 srun torchrun --nproc_per_node 1 /proj/nobackup/hpc2n2023-115/jailbreaker/chat.py \
