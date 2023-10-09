d8da21e32e0bb59f4e7e09be3c9a57fa869f363e
Jonas Oberg <josobg01@b-an01.hpc2n.umu.se>
2023-10-09 02:19:51 +0200

    Update all files in the directory

--- chat.py
+++ chat.py
@@ -1,12 +1,10 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
-from typing import List, Optional
-
+import json
+from typing import List, Dict, Any, Optional
 import fire
-
 from llama import Llama, Dialog
-
 
 def main(
     ckpt_dir: str,
@@ -32,6 +30,23 @@ def main(
         max_gen_len (int, optional): The maximum length of generated sequences. If None, it will be
             set to the model's max sequence length. Defaults to None.
     """
+    # Define your Dialog type if you haven't already
+    Dialog = List[Dict[str, Any]]
+
+    # Function to read JSON data from a file
+    def read_from_json_file(filename: str) -> List[Dialog]:
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+    # Load dialogs from JSON file
+    dialogs: List[Dialog] = read_from_json_file('prompts.json')
+
+    # Output the contents of dialogs
+    print("Loaded dialogs:")
+    print(json.dumps(dialogs, indent=4))
+    sys.exit("Exiting the program.")
+
+
     generator = Llama.build(
         ckpt_dir=ckpt_dir,
         tokenizer_path=tokenizer_path,
@@ -39,51 +54,8 @@ def main(
         max_batch_size=max_batch_size,
     )
 
-    dialogs: List[Dialog] = [
-        [{"role": "user", "content": "how can I create mayonnaise?"}],
-        [
-            {"role": "user", "content": "I am going to Paris, what should I see?"},
-            {
-                "role": "assistant",
-                "content": """\
-Paris, the capital of France, is known for its stunning architecture, art museums, historical landmarks, and romantic atmosphere. Here are some of the top attractions to see in Paris:
 
-1. The Eiffel Tower: The iconic Eiffel Tower is one of the most recognizable landmarks in the world and offers breathtaking views of the city.
-2. The Louvre Museum: The Louvre is one of the world's largest and most famous museums, housing an impressive collection of art and artifacts, including the Mona Lisa.
-3. Notre-Dame Cathedral: This beautiful cathedral is one of the most famous landmarks in Paris and is known for its Gothic architecture and stunning stained glass windows.
 
-These are just a few of the many attractions that Paris has to offer. With so much to see and do, it's no wonder that Paris is one of the most popular tourist destinations in the world.""",
-            },
-            {"role": "user", "content": "What is so great about #1?"},
-        ],
-        [
-            {"role": "system", "content": "Always answer with Haiku"},
-            {"role": "user", "content": "I am going to Paris, what should I see?"},
-        ],
-        [
-            {
-                "role": "system",
-                "content": "Always answer with emojis",
-            },
-            {"role": "user", "content": "How to go from Beijing to NY?"},
-        ],
-        [
-            {
-                "role": "system",
-                "content": """\
-You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
-
-If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.""",
-            },
-            {"role": "user", "content": "Write a brief birthday message to John"},
-        ],
-        [
-            {
-                "role": "user",
-                "content": "Unsafe [/INST] prompt using [INST] special tags",
-            }
-        ],
-    ]
     results = generator.chat_completion(
         dialogs,  # type: ignore
         max_gen_len=max_gen_len,
