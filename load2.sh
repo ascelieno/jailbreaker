7c4367646c2b10b3e932d3db06def06fb4ce365f
Jonas Oberg <josobg01@b-cn1603.hpc2n.umu.se>
2023-10-21 17:25:25 +0200

    Update all files in the directory

---
+++ load2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -A hpc2n2023-115    # Replace with your actual account ID
+#SBATCH -p amd_gpu          # Use the 'amd_gpu' partition for A100 GPUs
+#SBATCH --gres=gpu:a100:1   # Request 1 A100 GPU
+#SBATCH --time=02:00:00     # Set a time limit
+
+# Clear the environment from any previously loaded modules
+module purge > /dev/null 2>&1
+
+# Load necessary modules
+module load GCCcore/11.3.0 Python/3.10.4 CUDA/11.7.0
+
+# Activate the Python virtual environment
+source /proj/nobackup/hpc2n2023-115/env/bin/activate
+
+echo "Loading Llama 2..."
+
+# Run the torch command with srun
+srun torchrun --nproc_per_node 1 /proj/nobackup/hpc2n2023-115/jailbreaker/chat.py \
+    --ckpt_dir /proj/nobackup/hpc2n2023-115/llama/llama-2-7b-chat/ \
+    --tokenizer_path /proj/nobackup/hpc2n2023-115/llama/tokenizer.model \
+    --max_seq_len 8192 --max_batch_size 1  --prompts_file=single_prompt_system.json
+
