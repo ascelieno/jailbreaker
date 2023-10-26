d98d2a6bf9a0a91025c99d03ccf3f926489e93ac
Jonas Oberg <josobg01@b-cn1603.hpc2n.umu.se>
2023-10-26 23:43:56 +0200

    Update all files in the directory

---
+++ ollama_gen4.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Initialize an empty JSON array in the file
+echo "[" > responses.json
+
+# Read prompts from the file line by line
+while IFS= read -r prompt; do
+  # Make the query and capture the response
+  response=$(curl -s -X POST http://localhost:11434/api/generate -d "{
+    \"model\": \"llama2\",
+    \"prompt\":\"$prompt\",
+    \"stream\": false
+  }")
+
+  # Append the response to the JSON file and add a comma for separation
+  echo "$response," >> responses.json
+done < questions.txt
+
+# Use `sed` to remove the trailing comma from the last response
+sed -i '$ s/,$//' responses.json
+
+# Close the JSON array
+echo "]" >> responses.json
