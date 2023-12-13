2c2ab5ab7615ac2600b2da653ef068c272dcbe83
Jonas Oberg <josobg01@b-cn1603.hpc2n.umu.se>
2023-12-13 12:16:06 +0100

    Update all files in the directory

---
+++ qlora_tune_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -A hpc2n2023-124    # Replace with your actual account ID
+#SBATCH -p amd_gpu          # Use the 'amd_gpu' partition for A100 GPUs
+#SBATCH --gres=gpu:a100:1   # Request 1 A100 GPU
+#SBATCH --time=125:00:00     # Set a time limit
+
+ml GCCcore/11.3.0 Python/3.10.4
+
+cd /proj/nobackup/hpc2n2023-115/llm_qlora
+source venv/bin/activate
+python train.py configs/llama2_7b_uncensored_original.yaml
+
