9869855e49da50e165e6c812b7e0b454f0d299b0
Jonas Oberg <josobg01@b-cn1603.hpc2n.umu.se>
2024-05-10 11:44:22 +0200

    Update all files in the directory

---
+++ inference_example.sh
@@ -0,0 +1,1 @@
+./inference_hf.sh llama2-7b-instruction-tuned data/question_harmful.txt out --prompt --repetition_penalty=1.15 --max_length=1024 --temperature=0.6 --top_p=0.9
--- inference_hf.sh
+++ inference_hf.sh
@@ -5,6 +5,20 @@ ml GCCcore/11.3.0 Python/3.10.4
 
 source $PROJ/llm_qlora/new_env/bin/activate
 
+
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+echo $7
+echo $8
+
+
+echo ../jailbreaker/inference_hf.py configs/$1.yaml ../jailbreaker/$2 ../jailbreaker/$3 $4 $5 $6 $7 $8
+
+
 cd ../llm_qlora
 #python ../jailbreaker/inference_hf2.py $1 $2 $3 $4 $5 configs/$6.yaml ../jailbreaker/$7 ../jailbreaker/$8
 #python ../jailbreaker/inference_hf2.py --config_path configs/$1.yaml 
